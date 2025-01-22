3adf557d9f4fab3dbecca1dd7fb5a670d2ef78c1
Nimira43 <n.rabone72@aol.com>
2025-01-22 21:24:06 +0000

    Styled temporary HomePage

--- app/page.js
+++ app/page.js
@@ -1,13 +1,19 @@
-import Logo from '/logo.png'
+import Logo from '../images/logo.png'
 import Image from 'next/image'
 
 const HomePage = () => {
   return (
-    <div className='border-b-aegean-blue p-5'>
-      <h1 className='logo text-light'>Ephemeris</h1>
+    <div className='bg-wine-red p-5'>
+      <h1 className='logo text-herbal-green text-4xl font-semibold tracking-wide mb-4'>Ephemeris</h1>
       <hr />
-      <h3 className='heading'>Home Page</h3>
-      <Image src={Logo} alt='Logo' width={50} height={50} />
+      <h3 className='mt-4 mb-4 heading text-light'>Home Page</h3>
+      <Image 
+        src={Logo} 
+        alt='Logo' 
+        width={100} 
+        height={100}
+        className='rounded-full' 
+      />
     </div>
   )
 }
