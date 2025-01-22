e6e446f05e9dab5739630b49d68953d4006705bc
Nimira43 <n.rabone72@aol.com>
2025-01-22 21:09:54 +0000

    Cleaned up boilerplate. Brought colour themes into tailwind.config.js. Setup basic HomePage.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'primary-1': '#111',
+        'primary-2': '#222',
+        'primary-3': '#333',
+        'grey-dark': '#999',
+        'grey-light': '#ccc',
+        'lapis-lazuli': '#26619C',
+        'aegean-blue': '#2b8ead',
+        'wine-red': '#8a3a3a',
+        'terracotta': '#d98840',
+        'golden-olive': '#c5b358',
+        'herbal-green': '#7aa95d',
+        'marble-white': '#ededed',
+        'light': '#eeffee'
       },
     },
   },
   plugins: [],
-};
+}
