a314aebed493a541121d9d1ea5783dd2c4cf34ac
Nimira43 <n.rabone72@aol.com>
2025-05-14 21:18:18 +0100

    Fractal draw method

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ window.addEventListener('load', function () {
       this.canvasHeight = canvasHeight
       this.canvasWidth = canvasWidth
     }
-    
+    draw(context) {
+      context.beginPath()
+      context.moveTo(0, 0)
+      context.lineTo(canvas.width, canvas.height)
+      context.stroke()
+    }
   }
 
   class Particle {
