152a4acaf371f95c715ab274eb8263593915b535
Nimira43 <n.rabone72@aol.com>
2025-05-14 21:35:44 +0100

    Adding skyblue to #canvas in CSS

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.addEventListener('load', function () {
     }
 
     draw(context) {
-      context.fillRect(0, 0, this.canvasWidth, this.canvasHeight)
+      context.fillRect(50, 0, this.canvasWidth, this.canvasHeight)
       context.beginPath()
       context.moveTo(0, 0)
       context.lineTo(this.size, 0)
