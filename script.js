d6357849f434ef6c446c5338563c4e7feea995df
Nimira43 <n.rabone72@aol.com>
2025-05-14 22:01:07 +0100

    Private methods have code that's hidden - abstraction - hiding unnescessary details of the inner workings of objects from the outside and only exposing the essentials

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ window.addEventListener('load', function () {
       context.save()
       context.translate(this.canvasWidth / 2, this.canvasHeight / 2)
       context.scale(1, 1)
-      context.rotate(0)
+      context.rotate(0.8)
       context.beginPath()
       context.moveTo(0, 0)
       context.lineTo(this.size, 0)
