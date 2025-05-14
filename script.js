a1328ebe411ce9e518768d845a15fb41eb61d056
Nimira43 <n.rabone72@aol.com>
2025-05-14 21:21:10 +0100

    Created fractal1 an instance of Fractal class

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ window.addEventListener('load', function () {
       this.canvasHeight = canvasHeight
       this.canvasWidth = canvasWidth
     }
+
     draw(context) {
       context.beginPath()
       context.moveTo(0, 0)
@@ -16,6 +17,9 @@ window.addEventListener('load', function () {
       context.stroke()
     }
   }
+
+  const fractal1 = new Fractal(canvas.width, canvas.height)
+  fractal1.draw(ctx)
 
   class Particle {
 
