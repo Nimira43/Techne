a0383a894b09d67b51ec06c3ac85e4350f421803
Nimira43 <n.rabone72@aol.com>
2025-05-15 10:06:34 +0100

    Chnged value of branches.

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ window.addEventListener('load', function () {
       this.maxLevel = 3                     // Change
       this.scale = 0.6                      // Change
       this.spread = 1                       // Change 
-      this.branches = 1
+      this.branches = 10                    // Change
     }
 
     draw(context) {
