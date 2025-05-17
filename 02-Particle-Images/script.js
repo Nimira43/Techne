cca27461efc6cac84edba4fabdaf8c0449ce7663
Nimira43 <n.rabone72@aol.com>
2025-05-17 03:49:41 +0100

    Setting up HTML

--- 02-Particle-Images/script.js
+++ 02-Particle-Images/script.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', function () {
-  const canvas = this.document.getElementById('canvas1')
-  const ctx = canvas.getContext('2d')
-  canvas.width = 600
-  canvas.height = 600
-})
+// window.addEventListener('load', function () {
+//   const canvas = this.document.getElementById('canvas1')
+//   const ctx = canvas.getContext('2d')
+//   canvas.width = 600
+//   canvas.height = 600
+// })
