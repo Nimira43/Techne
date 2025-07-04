d8b863198779b2915987ad79185241124c8818fd
Nimira43 <n.rabone72@aol.com>
2025-07-05 00:00:50 +0100

    Setting out Particle class

--- 01-Text/script.js
+++ 01-Text/script.js
@@ -6,61 +6,73 @@ window.addEventListener('load', function () {
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight 
 
-  ctx.lineWidth = 3
-  ctx.strokeStyle = '#006400' 
-  ctx.beginPath()
-  ctx.moveTo(canvas.width / 2, 0)
-  ctx.lineTo(canvas.width / 2, canvas.height)
-  ctx.stroke()
-  
-  ctx.strokeStyle = '#006400' 
-  ctx.beginPath()
-  ctx.moveTo(0, canvas.height / 2)
-  ctx.lineTo(canvas.width, canvas.height / 2)
-  ctx.stroke()
+  class Particle {
+    constructor() {
 
-  const gradient = ctx.createLinearGradient(
-    0, 0, canvas.width, canvas.height
-  )
-  gradient.addColorStop(0.3, '#ffd700')
-  gradient.addColorStop(0.5, '#ff4500')
-  gradient.addColorStop(0.7, '#ff0000')
-  ctx.fillStyle = gradient
-  ctx.strokeStyle = '#ffd700'
-  ctx.font = '80px Helvetica'
-  ctx.textAlign = 'center'
-  ctx.textBaseline = 'middle'
-  
-  const maxTextWidth = canvas.width * 0.8
-  const lineHeight = 80
-
-  function wrapText(text) {
-    let linesArray = []
-    let lineCounter = 0
-    let line = ''
-    let words = text.split(' ')
-
-    for (let i = 0; i < words.length; i++) {
-      let testLine = line + words[i] + ' '
-      if (ctx.measureText(testLine).width > maxTextWidth) {
-        line = words[i] + ' '
-        lineCounter++
-      } else {
-        line = testLine
-      }
-      linesArray[lineCounter] = line 
     }
-    let textHeight = lineHeight * lineCounter
-    let textY = canvas.height / 2 - textHeight / 2
-    linesArray.forEach((el, index) => {
-      ctx.fillText(el, canvas.width / 2, textY + (index * lineHeight))
-    })
-    console.log(linesArray)
+    draw() {
+
+    }
+    update() {
+      
+    }
   }
 
-  textInput.addEventListener('keyup', function(e) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-    wrapText(e.target.value)
-  })
+  // ctx.lineWidth = 3
+  // ctx.strokeStyle = '#006400' 
+  // ctx.beginPath()
+  // ctx.moveTo(canvas.width / 2, 0)
+  // ctx.lineTo(canvas.width / 2, canvas.height)
+  // ctx.stroke()
+  
+  // ctx.strokeStyle = '#006400' 
+  // ctx.beginPath()
+  // ctx.moveTo(0, canvas.height / 2)
+  // ctx.lineTo(canvas.width, canvas.height / 2)
+  // ctx.stroke()
+
+  // const gradient = ctx.createLinearGradient(
+  //   0, 0, canvas.width, canvas.height
+  // )
+  // gradient.addColorStop(0.3, '#ffd700')
+  // gradient.addColorStop(0.5, '#ff4500')
+  // gradient.addColorStop(0.7, '#ff0000')
+  // ctx.fillStyle = gradient
+  // ctx.strokeStyle = '#ffd700'
+  // ctx.font = '80px Helvetica'
+  // ctx.textAlign = 'center'
+  // ctx.textBaseline = 'middle'
+  
+  // const maxTextWidth = canvas.width * 0.8
+  // const lineHeight = 80
+
+  // function wrapText(text) {
+  //   let linesArray = []
+  //   let lineCounter = 0
+  //   let line = ''
+  //   let words = text.split(' ')
+
+  //   for (let i = 0; i < words.length; i++) {
+  //     let testLine = line + words[i] + ' '
+  //     if (ctx.measureText(testLine).width > maxTextWidth) {
+  //       line = words[i] + ' '
+  //       lineCounter++
+  //     } else {
+  //       line = testLine
+  //     }
+  //     linesArray[lineCounter] = line 
+  //   }
+  //   let textHeight = lineHeight * lineCounter
+  //   let textY = canvas.height / 2 - textHeight / 2
+  //   linesArray.forEach((el, index) => {
+  //     ctx.fillText(el, canvas.width / 2, textY + (index * lineHeight))
+  //   })
+  //   console.log(linesArray)
+  // }
+
+  // textInput.addEventListener('keyup', function(e) {
+  //   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  //   wrapText(e.target.value)
+  // })
 })
 
