9860d7e8d3a4d28d5a5fab6a8c478cc588ce0f19
Nimira43 <n.rabone72@aol.com>
2025-07-04 22:03:15 +0100

    Draw line

--- 01-Text/script.js
+++ 01-Text/script.js
@@ -1,9 +1,14 @@
 window.addEventListener('load', function () {
   const canvas = document.getElementById('canvas1')
   const ctx = canvas.getContext('2d')
-  
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight 
+
+  ctx.strokeStyle = '#fff' 
+  ctx.beginPath()
+  ctx.moveTo(100, 100)
+  ctx.lineTo(200, 300)
+  ctx.stroke()
 
   const text = 'Text Animations'
   const textX = canvas.width / 2
