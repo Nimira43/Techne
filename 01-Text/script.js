f7e2bf1789ca197dd883573632fb8a2da2b594b4
Nimira43 <n.rabone72@aol.com>
2025-07-04 23:23:37 +0100

    Creating textInput variable.

--- 01-Text/script.js
+++ 01-Text/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('load', function () {
+
+  const textInput = this.document.getElementById('textInput')
   const canvas = document.getElementById('canvas1')
   const ctx = canvas.getContext('2d')
   canvas.width = window.innerWidth
