df91ae601f393846f604fbffe1ce52bb7a891050
Nimira43 <n.rabone72@aol.com>
2025-07-04 22:10:48 +0100

    Added horizontal line.

--- 01-Text/script.js
+++ 01-Text/script.js
@@ -5,20 +5,26 @@ window.addEventListener('load', function () {
   canvas.height = window.innerHeight 
 
   ctx.lineWidth = 3
-  ctx.strokeStyle = '#fff' 
+  ctx.strokeStyle = '#006400' 
   ctx.beginPath()
   ctx.moveTo(canvas.width / 2, 0)
   ctx.lineTo(canvas.width / 2, canvas.height)
   ctx.stroke()
+  
+  ctx.strokeStyle = '#006400' 
+  ctx.beginPath()
+  ctx.moveTo(0, canvas.width / 2)
+  ctx.lineTo(canvas.width, canvas.height / 2)
+  ctx.stroke()
 
-  const text = 'Text Animations'
+  const text = 'NimiraTech'
   const textX = canvas.width / 2
   const textY = canvas.height / 2
   ctx.fillStyle = '#ff4500'
   ctx.strokeStyle = '#ffd700'
   
   ctx.font = '80px Verdana'
-  ctx.textAlign = 'end'
+  ctx.textAlign = 'center'
   ctx.fillText(text, textX, textY)
   ctx.strokeText(text, textX, textY)
 })
