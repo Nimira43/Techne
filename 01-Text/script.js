3d3e0b1e8c738e4083aa5262727fd8097e0e0a45
Nimira43 <n.rabone72@aol.com>
2025-07-04 21:19:42 +0100

    Display text

--- 01-Text/script.js
+++ 01-Text/script.js
@@ -4,4 +4,14 @@ window.addEventListener('load', function () {
   
   canvas.width = window.innerWidth
   canvas.height = window.innerHeight 
+
+  const text = 'Text Animations'
+  const textX = 150
+  const textY = 150
+  ctx.fillStyle = '#ff4500'
+  ctx.strokeStyle = '#ffd700'
+  ctx.lineWidth = 3
+  ctx.font = '80px Helvetica'
+  ctx.fillText(text, textX, textY)
+  ctx.strokeText(text, textX, textY)
 })
