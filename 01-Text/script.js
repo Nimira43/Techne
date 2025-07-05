978320b102a3f4dacec7b73d27bea1929c2dec55
Nimira43 <n.rabone72@aol.com>
2025-07-05 02:51:29 +0100

    Calling effect.render

--- 01-Text/script.js
+++ 01-Text/script.js
@@ -129,7 +129,7 @@ window.addEventListener('load', function () {
 
   const effect = new Effect(ctx, canvas.width, canvas.height)
   effect.wrapText('NimiraTech')
-  
+  effect.render()
   
 
   function animate() {
