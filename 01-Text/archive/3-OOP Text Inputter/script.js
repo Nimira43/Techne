278ed188fee94947f6fde7ca9a8033ee05d417ef
Nimira43 <n.rabone72@aol.com>
2025-07-05 01:41:01 +0100

    Archiving progress

---
+++ 01-Text/archive/3-OOP Text Inputter/script.js
@@ -0,0 +1,91 @@
+window.addEventListener('load', function () {
+  const canvas = document.getElementById('canvas1')
+  const ctx = canvas.getContext('2d')
+  canvas.width = window.innerWidth
+  canvas.height = window.innerHeight 
+
+  class Particle {
+    constructor() {
+
+    }
+    draw() {
+
+    }
+    update() {
+      
+    }
+  }
+  
+  class Effect {
+    constructor(context, canvasWidth, canvasHeight) {
+      this.context = context
+      this.canvasWidth = canvasWidth
+      this.canvasHeight = canvasHeight
+      this.textX = this.canvasWidth / 2
+      this.textY = this.canvasHeight / 2
+      this.fontSize = 100
+      this.lineHeight = this.fontSize * 0.8
+      this.maxTextWidth = this.canvasWidth * 0.8
+      this.textInput = document.getElementById('textInput')
+      this.textInput.addEventListener('keyup', (e) => {  
+        if (e.key !== ' ') {
+          this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight)
+          this.wrapText(e.target.value)
+        }
+      })
+    }
+    wrapText(text) {
+      const gradient = this.context.createLinearGradient(
+        0, 0, this.canvasWidth, this.canvasHeight
+      )
+      gradient.addColorStop(0.3, '#ffd700')
+      gradient.addColorStop(0.5, '#ff4500')
+      gradient.addColorStop(0.7, '#ff0000')
+      
+      this.context.fillStyle = gradient
+      this.context.textAlign = 'center'
+      this.context.textBaseline = 'middle'
+      this.context.lineWidth = 3
+      this.context.strokeStyle = '#0000ff'
+      this.context.font = this.fontSize + 'px Verdana'
+       
+      let linesArray = []
+      let words = text.split(' ')
+      let lineCounter = 0
+      let line = ''
+
+      for (let i = 0; i < words.length; i++) {
+        let testLine = line + words[i] + ' '
+        if (this.context.measureText(testLine).width > this.maxTextWidth) {
+          line = words[i] + ' '
+          lineCounter++
+        } else {
+          line = testLine
+        }
+        linesArray[lineCounter] = line 
+      }  
+      let textHeight = this.lineHeight * lineCounter
+      this.textY = this.canvasHeight / 2 - textHeight / 2
+      
+      linesArray.forEach((el, index) => {
+        this.context.fillText(el, this.textX, this.textY + (index * this.lineHeight))
+        this.context.strokeText(el, this.textX, this.textY + (index * this.lineHeight))      
+      })
+    }
+    convertToParticles() {
+
+    }
+    render() {
+
+    }
+  }
+
+  const effect = new Effect(ctx, canvas.width, canvas.height)
+  effect.wrapText('NimiraTech')
+  console.log(effect)
+
+  function animate() {
+
+  }  
+})
+
