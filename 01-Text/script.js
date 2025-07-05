2b717458a713db081ba0057c6401da020cc49544
Nimira43 <n.rabone72@aol.com>
2025-07-05 01:21:07 +0100

    Correcting error on line 29 - 8 should be 0.8. Refactor continuing

--- 01-Text/script.js
+++ 01-Text/script.js
@@ -26,7 +26,7 @@ window.addEventListener('load', function () {
       this.textX = this.canvasWidth / 2
       this.textY = this.canvasHeight / 2
       this.fontSize = 100
-      this.lineHeight = this.fontSize * 8
+      this.lineHeight = this.fontSize * 0.8
       this.maxTextWidth = this.canvasWidth * 0.8
     }
     wrapText(text) {
@@ -36,15 +36,16 @@ window.addEventListener('load', function () {
       gradient.addColorStop(0.3, '#ffd700')
       gradient.addColorStop(0.5, '#ff4500')
       gradient.addColorStop(0.7, '#ff0000')
+      
       this.context.fillStyle = gradient
       this.context.textAlign = 'center'
       this.context.textBaseline = 'middle'
       this.context.lineWidth = 3
       this.context.strokeStyle = '#0000ff'
       this.context.font = this.fontSize + 'px Verdana'
-      this.context.fillText(text, this.textX, this.textY)
-      this.context.strokeText(text, this.textX, this.textY)
-
+      // this.context.fillText(text, this.textX, this.textY)
+      // this.context.strokeText(text, this.textX, this.textY)
+      
       let linesArray = []
       let words = text.split(' ')
       let lineCounter = 0
@@ -52,28 +53,27 @@ window.addEventListener('load', function () {
 
       for (let i = 0; i < words.length; i++) {
         let testLine = line + words[i] + ' '
-        if (this.context.measureText(testLine).width > maxTextWidth) {
+        if (this.context.measureText(testLine).width > this.maxTextWidth) {
           line = words[i] + ' '
           lineCounter++
         } else {
           line = testLine
         }
-      }
+        linesArray[lineCounter] = line 
+      }  
+      let textHeight = this.lineHeight * lineCounter
+      this.textY = this.canvasHeight / 2 - textHeight / 2
+      
+      linesArray.forEach((el, index) => {
+        this.context.fillText(el, this.textX, this.textY + (index * this.lineHeight))
+        this.context.strokeText(el, this.textX, this.textY + (index * this.lineHeight))      
+      })
     }
              
-      //     linesArray[lineCounter] = line 
-      //   }
-      //   let textHeight = lineHeight * lineCounter
-      //   let textY = canvas.height / 2 - textHeight / 2
-      //   linesArray.forEach((el, index) => {
-      //     ctx.fillText(el, canvas.width / 2, textY + (index * lineHeight))
-      //   })
-      //   console.log(linesArray)
-      // }
-      // textInput.addEventListener('keyup', function(e) {
-      //   ctx.clearRect(0, 0, canvas.width, canvas.height)
-      //   wrapText(e.target.value)
-      // })
+    // textInput.addEventListener('keyup', function(e) {
+    //   ctx.clearRect(0, 0, canvas.width, canvas.height)
+    //   wrapText(e.target.value)
+    // })
     
     convertToParticles() {
 
